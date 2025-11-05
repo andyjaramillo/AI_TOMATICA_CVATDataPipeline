98a0c483d4a213d3cff049b39f5c3dc05808b1c2
Jason Chamorro <jchamor1@lion.lmu.edu>
2025-11-05 12:53:20 +0100

    updated to work with multiple files

--- save.py
+++ save.py
@@ -37,56 +37,59 @@ if __name__ == '__main__':
     folder_path = "data"
     entries = os.listdir(folder_path)
     # Filter out directories, keeping only files (optional, but good practice)
-    filename = [entry for entry in entries if os.path.isfile(os.path.join(folder_path, entry))][0]
-    cap = cv2.VideoCapture(f'data/{filename}')
-    cap_fps = cap.get(cv2.CAP_PROP_FPS)
-    num_frames = imageio.get_reader(f'data/{filename}', 'ffmpeg').count_frames()
-    print(num_frames)
+    filenames = [entry for entry in entries if os.path.isfile(os.path.join(folder_path, entry))]
+
     if os.path.exists("output") == False:
         os.mkdir("output")
-    #cv2.namedWindow('FRAME')
-
-    # Initialize MultiObjectTracker
-    tracker = MultiObjectTracker(dt=1 / cap_fps, tracker_kwargs={'max_staleness': 10})
-
-    # Initialize ID dictionary and counter
-    id_dict = {}
-    j = 0
-    count = 0
-    for i in tqdm(range(num_frames)):
-        ret, frame = cap.read()
-        frame = cv2.resize(frame, (1020, 500))
-        results = model(frame)
-        output = results.pandas().xyxy[0]
-
-        # Filter objects with label "person"
-        objects = output[output['name'] == 'person']
+    for filename in filenames:
+        cap = cv2.VideoCapture(f'data/{filename}')
+        cap_fps = cap.get(cv2.CAP_PROP_FPS)
+        num_frames = imageio.get_reader(f'data/{filename}', 'ffmpeg').count_frames()
+        print(num_frames)
         
-        detections = []
-        #print(detections)
-        # Pass YOLO detections to motpy
-        for index, obj in objects.iterrows():
-            coordinates = [int(obj['xmin']), int(obj['ymin']), int(obj['xmax']), int(obj['ymax'])]
-            detections.append(Detection(box=coordinates, score=obj['confidence'], class_id=obj['class']))
-        
-        if len(detections) > 0:
+        #cv2.namedWindow('FRAME')
+
+        # Initialize MultiObjectTracker
+        tracker = MultiObjectTracker(dt=1 / cap_fps, tracker_kwargs={'max_staleness': 10})
+
+        # Initialize ID dictionary and counter
+        id_dict = {}
+        j = 0
+        count = 0
+        for i in tqdm(range(num_frames)):
+            ret, frame = cap.read()
+            frame = cv2.resize(frame, (1020, 500))
+            results = model(frame)
+            output = results.pandas().xyxy[0]
+
+            # Filter objects with label "person"
+            objects = output[output['name'] == 'person']
             
-            cv2.imwrite(f'output/image_{count}.png', frame)
-            print(f"wrote file image_{count}") 
-            count += 1
-        # Perform object tracking
-        #tracker.step(detections=detections)
-        #track_results = tracker.active_tracks()
-        
-        # Update ID dictionary
-        #id_dict, j = update_id_dict(id_dict, j, track_results)
+            detections = []
+            #print(detections)
+            # Pass YOLO detections to motpy
+            for index, obj in objects.iterrows():
+                coordinates = [int(obj['xmin']), int(obj['ymin']), int(obj['xmax']), int(obj['ymax'])]
+                detections.append(Detection(box=coordinates, score=obj['confidence'], class_id=obj['class']))
+            
+            if len(detections) > 0:
+                
+                cv2.imwrite(f'output/image_{count}.png', frame)
+                print(f"wrote file image_{count}") 
+                count += 1
+            # Perform object tracking
+            #tracker.step(detections=detections)
+            #track_results = tracker.active_tracks()
+            
+            # Update ID dictionary
+            #id_dict, j = update_id_dict(id_dict, j, track_results)
 
-        # Draw bounding boxes on frame
-        #draw_boxes(frame, track_results)
-        #cv2.imshow('FRAME', frame)
+            # Draw bounding boxes on frame
+            #draw_boxes(frame, track_results)
+            #cv2.imshow('FRAME', frame)
 
-        if cv2.waitKey(1) & 0xFF == 27:
-            break
+            if cv2.waitKey(1) & 0xFF == 27:
+                break
 
-    cap.release()
+        cap.release()
     cv2.destroyAllWindows()
